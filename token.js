64773898c0ce637451c04cd1ff92e51fdb9a2ce3
d1ko <58879685+d1ko@users.noreply.github.com>
2024-02-08 21:37:26 +0600

    Ваше сообщение коммита здесь

--- token.js
+++ token.js
@@ -1,4 +1,4 @@
-const TOKEN_API = "6715329685:AAHKMQLnM8sdzsWZpi-8l1YOYbWHaEdfhX8";
+const TOKEN_API = "6585687387:AAHEyeqgm0OVdh9bnAvInE24bNbA1I58Q0A";
 const GOOGLE_API = "1NvwOMNVS-01Rb7fKAUk8wbmHohmY8l0r9lgPsF4eXlw";
 module.exports = {
     TOKEN_API,
